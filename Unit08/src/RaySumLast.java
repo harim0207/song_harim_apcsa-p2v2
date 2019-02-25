993cfeccfa45fefb3a48de75e7a28739f285a596
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-02-25 09:58:01 -0800

    unit8 done

--- Unit08/src/RaySumLast.java
+++ Unit08/src/RaySumLast.java
@@ -7,6 +7,13 @@ public class RaySumLast
 {
 	public static int go(int[] ray)
 	{
-		return 0;
+		int sum = 0;
+		for(int spot = 0; spot<ray.length-1; spot ++)
+		{
+			if (ray[spot]> ray[ray.length-1]) sum += ray[spot];
+		}
+		if (sum != 0)
+			return sum;
+		else return -1;
 	}
 }
