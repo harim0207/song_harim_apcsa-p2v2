7e199a6f52c10a984fd798c8ccaa56d6c8188d49
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-05 10:57:23 -0800

    unit09 arraylists in progress

--- Unit09-ArrayLists/src/DownRunner.java
+++ Unit09-ArrayLists/src/DownRunner.java
@@ -2,10 +2,18 @@
 //www.apluscompsci.com
 //Name -
 //Date -
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DownRunner
 {
 	public static void main( String args[] )
-	{											
+	{	
+		List<Integer> numArray = Arrays.asList(-99,1,2,3,4,5,6,7,8,9,10,12345);
+		ListDown test =ListDown(numArray);
+		System.out.println(test.go());
+		
 	}
+
 }
--- Unit09-ArrayLists/src/ListDown.java
+++ Unit09-ArrayLists/src/ListDown.java
@@ -10,8 +10,38 @@ public class ListDown
 {
 	//go() will return true if all numbers in numArray
 	//are in decreasing order [31,12,6,2,1]
-	public static boolean go(List<Integer> numArray)
+	private List<Integer> array = new ArrayList<Integer>();
+	public ListDown()
 	{
+		array = new ArrayList<Integer>();
+	}
+	
+	public ListDown(List<Integer> numArray)
+	{
+		setRay(numArray);
+	}
+	
+	public void setRay(List<Integer> numArray)
+	{
+		for(int i = 0; i<numArray.size(); i++)
+		{
+			array.add(i, numArray.get(i));
+		}
+	}
+	
+	public /*static*/ boolean go(/*List<Integer> numArray*/)
+	{
+		for(int i = 0; i<array.size()-1; i++)
+		{
+			if(array.get(i)> array.get(i+1))
+				continue;
+			else return false;
+		}
 		return true;
 	}	
+	
+	public String toString()
+	{
+		return "" + go(/*array*/);
+	}
 }
