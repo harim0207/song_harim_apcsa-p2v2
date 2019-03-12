df5ad13b5393ffabebab228d9f4b271b977c03ba
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-12 10:01:50 -0700

    arraylist elevens

---
+++ Unit09/src/arraylistselevens/DeckTester.java
@@ -0,0 +1,33 @@
+package arraylistselevens;
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		Deck test = new Deck(new String[] {"jack","queen","king"}, 
+				new String[] {"blue", "red"}, 
+				new int[] {11, 12, 13});
+		System.out.println(test.isEmpty());
+		System.out.println(test.deal());
+		System.out.println(test);
+		
+		Deck test2 = new Deck(new String[] {"Ace", "Two", "Three"}, 
+				new String[] {"red", "black"}, 
+				new int[] {1, 2, 3});
+		System.out.println(test2.deal());
+		System.out.println(test2);
+
+		Deck test3 = new Deck(new String[] {"Jack", "Queen", "King", "Ace"},
+				new String[] {"Spades", "Hearts", "Diamonds", "Clubs"},
+				new int[] {10, 10, 10, 1});
+		System.out.println(test3.deal());
+		System.out.println(test3);
+		
+	}
+}
