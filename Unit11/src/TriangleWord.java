ba322666c9f3b02a05041dd990df97abfc1413a7
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-13 11:52:57 -0700

    triangle in progress

--- Unit11/src/TriangleWord.java
+++ Unit11/src/TriangleWord.java
@@ -8,8 +8,31 @@ import static java.lang.System.*;
 public class TriangleWord
 {
 	//instance variables and constructors could be present, but are not necessary
-		
-	public static void printTriangle(String word)
+	private String a;
+	
+	public TriangleWord()
 	{
+		setWord("");
+	}
+	public TriangleWord(String word)
+	{
+		setWord(word);
+	}
+	
+	public void setWord(String word)
+	{
+		a = word;
+	}
+	public void printTriangle(/*String word*/)
+	{
+		String output = "";
+		for(int i =0; i < a.length(); i++)//times printed
+		{
+			for(int j = 1; j<i; j++)//letters
+			{
+				output += output + a.substring(0, j) + "\n"; 
+			}
+		}
+		out.println(output);
 	}
 }
--- Unit11/src/TriangleWordRunner.java
+++ Unit11/src/TriangleWordRunner.java
@@ -9,6 +9,12 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
+		Scanner keyboard= new Scanner(System.in);
+		TriangleWord test = new TriangleWord();
 		
+		out.println("Enter a word: ");
+		String word = keyboard.next();
+		test.setWord(word);
+		test.printTriangle();
 	}
 }
