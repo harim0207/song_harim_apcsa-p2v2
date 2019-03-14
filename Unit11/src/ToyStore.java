ea5d82c6eaf446ba18713b82be41228af2012816
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-13 23:36:33 -0700

    toystore in progress

--- Unit11/src/ToyStore.java
+++ Unit11/src/ToyStore.java
@@ -14,15 +14,47 @@ public class ToyStore
 
 	public ToyStore()
 	{
+		toyList= new ArrayList<Toy>();
+		loadToys("");
+	}
+	
+	public ToyStore(String toys)
+	{
+		toyList = new ArrayList<Toy>();
+		loadToys(toys);
 	}
 
 	public void loadToys( String toys )
 	{
+		String[] ToyArr = toys.split(" ");
+		String toyName = "";
+		boolean exists = false;
+		for(int i = 0; i < ToyArr.length; i++)
+		{
+			exists = false;
+			for (int j = 0; j < toyList.size(); j++)
+			{
+				if (ToyArr[i].equals(toyList.get(j).getName()))
+				{
+					exists = true;
+					toyList.get(j).setCount(toyList.get(j).getCount() + 1);
+					break;
+				}
+			}
+			
+			if (exists == false )
+			{
+				toyName = ToyArr[i];
+				toyList.add(new Toy(toyName));
+				
+			}
+		}
 	}
   
   	public Toy getThatToy( String nm )
   	{
-  		return null;
+
+  		return ;
   	}
   
   	public String getMostFrequentToy()
