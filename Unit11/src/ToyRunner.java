bf21db5b081fa504b144a25670c383e60f7d995d
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-13 20:14:14 -0700

    toy in progress

--- Unit11/src/ToyRunner.java
+++ Unit11/src/ToyRunner.java
@@ -3,10 +3,30 @@
 //Name -
 
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class ToyRunner
 {
 	public static void main(String[] args)
 	{
+		Scanner keyboard = new Scanner(System.in);
+		Toy test = new Toy();
+		String choice = "y";
+		
+		do {
+			out.println("Enter a toy: ");
+			String toy = keyboard.next();
+			test.setName(toy);
+			
+			out.println("Enter the count: ");
+			int count = keyboard.nextInt();
+			test.setCount(count);
+			
+			out.println(test);
+			out.print("Do you want to add more toys? ");
+			choice = keyboard.next();
+		}while ( choice.equals("Y")||choice.contentEquals("y"));
+		
+		
 	}
 }
