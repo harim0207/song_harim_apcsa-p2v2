592fc7270be6b292f264714bfb8cf0eafba4752d
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-18 11:06:59 -0700

    monster in progress

--- Unit12/Skeleton.java
+++ Unit12/Skeleton.java
@@ -6,11 +6,47 @@ import static java.lang.System.*;
 
 public class Skeleton implements Monster
 {
-	//add instance variables	
-
-	//add a constructor
-
-	//add code to implement the Monster interface
-
-	//add a toString
+	private int size;
+	private String name;
+	public Skeleton()
+	{
+		name = "";
+		size = 0;
+	}
+	
+	public Skeleton(String n, size s;
+	{
+		name = n;
+		size = s;
+	}
+	
+	public int getHowBig() 
+	{
+		return size;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public boolean isBigger(Monster other)
+	{
+		return size > other.getHowBig();
+	}
+	
+	public boolean isSmaller(Monster other)
+	{
+		return size < other.getHowBig();
+	}
+	
+	public boolean namesTheSame(Monster other)
+	{
+		return name.equals(other.getName());
+	}
+	
+	public String toString()
+	{
+		return name + "" + size;
+	}
 }
