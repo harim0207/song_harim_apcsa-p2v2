42ad80c5650a233a65053939178b9041c9cb4558
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-25 10:31:43 -0700

    totalrow done

--- Unit14/src/TotalRow.java
+++ Unit14/src/TotalRow.java
@@ -6,9 +6,34 @@ import java.util.*;
 
 public class TotalRow
 {
-    public static List<Integer> getRowTotals( int[][] m )
+	private int[][]array;
+	public TotalRow()
+	{
+
+	}
+	
+	public TotalRow(int[][] m)
+	{
+
+	}
+	
+	public void setRowTotals(int[][] m)
+	{
+		array = m;
+	}
+    public static List<Integer> getRowTotals( int [][] m )
     {
-    	//add code here
-		return null;
+    	List<Integer> ListSum = new ArrayList<Integer>();
+    	int sum =0;
+    	for(int i = 0; i< m.length; i++)
+    	{
+    		sum = 0;
+    		for(int j = 0; j< m[i].length; j++)
+    		{
+    			sum += m[i][j];
+    		}
+   			ListSum.add(sum);
+    	}
+		return ListSum;
     }
 }
--- Unit14/src/TotalRowRunner.java
+++ Unit14/src/TotalRowRunner.java
@@ -6,7 +6,13 @@ public class TotalRowRunner
 {
 	public static void main( String args[] ) throws Exception
 	{
-		//add code here			
+		TotalRow test = new TotalRow();
+		int[][]list = {{1,2,3},{5,5,5,5}};
+		System.out.println(test.getRowTotals(list));
+		int[][]list1 = {{1,2,3},{5},{1},{2,2}};
+		System.out.println(test.getRowTotals(list1));
+		int[][]list2 = {{1,2},{5,5},{5,5},{4,5,6,7},{123124,12312}};
+		System.out.println(test.getRowTotals(list2));
 	}
 }
 
