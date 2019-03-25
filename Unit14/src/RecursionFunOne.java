29a1e1cd687eeac40404663b10852192bf39d4af
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-03-25 10:06:54 -0700

    recursion done

--- Unit14/src/RecursionFunOne.java
+++ Unit14/src/RecursionFunOne.java
@@ -8,8 +8,15 @@ public class RecursionFunOne
 {
 	public static int countOddDigits(int num)
 	{
+		int count = 0;
+		while( num!= 0)
+		{
+			int digit = num%10;
+			if(digit%2 == 0) count ++;
+			num= num/10;
+			countOddDigits(num);
+		}
 
-
-		return 0;
+		return count;
 	}
 }
