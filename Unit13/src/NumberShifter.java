839a5b72bab4ea8c83b36d6fcce5a8a780be02df
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-04-02 10:57:08 -0700

    numbershifter in progress

--- Unit13/src/NumberShifter.java
+++ Unit13/src/NumberShifter.java
@@ -3,6 +3,7 @@
 //Name -
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -10,11 +11,48 @@ import static java.lang.System.*;
 
 public class NumberShifter
 {
-	public static int[] makeLucky7Array( int size)
+	private ArrayList<Integer> list = new ArrayList<Integer>();
+	private int[] sorted;
+	
+	public NumberShifter()
 	{
-		return null;
+	
 	}
-	public static void shiftEm(int[] array)
+	public int[] makeLucky7Array( int size)
 	{
+		sorted = new int[size];
+		for(int i = 0; i < size; i++)
+		{
+			sorted[i] = list.get(i);
+		}
+		return sorted;
+	}
+	public void shiftEm(int[] array)
+	{
+		Arrays.sort(array);
+		int count7 =0;
+		for(int i=0; i<array.length; i++)
+		{
+			list.add(array[i]);
+			if(array[i] == 7)
+			{
+				count7 ++; 
+			}
+		}
+		for(int i :array)
+		{
+		int index = list.indexOf(7);
+		if(index != -1) list.remove(index);
+		}
+		
+		for(int j = 0; j<count7; j++)
+		{
+			list.add(0, 7);
+		}
+	}
+	
+	public String toString()
+	{
+		return Arrays.toString(sorted);
 	}
 }
--- Unit13/src/NumberShifterRunner.java
+++ Unit13/src/NumberShifterRunner.java
@@ -13,6 +13,10 @@ public class NumberShifterRunner
 	public static void main( String args[] ) throws IOException
 	{
 		//add test cases
+		NumberShifter test = new NumberShifter();
+		test.shiftEm(new int[]{7,1,5,3,11,5,6,7,8,9,10,12345,11});
+		out.println(test.makeLucky7Array(13));
+
 	}
 }
 
