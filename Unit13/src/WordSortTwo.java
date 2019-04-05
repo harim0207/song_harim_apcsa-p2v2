b6c160da4311784a2bd9037b69339d3e4fecd1b3
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-04-05 01:04:57 -0700

    sort2 and search done

--- Unit13/src/WordSortTwo.java
+++ Unit13/src/WordSortTwo.java
@@ -12,15 +12,34 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
-	}
+		wordRay = sentence.split(" ");
+	}	
+		
 
 	public void sort()
 	{
+		for (int i = 0; i < wordRay.length; i++)
+		{
+			for(int j = i+1; j < wordRay.length; j ++)
+			{
+				if (wordRay[i].compareTo(wordRay[j]) > 0)
+				{
+					String setter = "";
+					setter = wordRay[i];
+					wordRay[i] = wordRay[j];
+					wordRay[j] = setter;
+				}
+			}
+		}
 	}
 
 	public String toString()
 	{
-		String output="";
+		String output = "";
+		for (String word: wordRay)
+		{
+			output = output + "\n" + word;
+		}
 		return output+"\n\n";
 	}
 }
