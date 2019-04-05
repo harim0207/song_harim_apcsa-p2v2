bace705c2ffff22485189d63cd2a22cfcdd20cbe
harim0207 <38955649+harim0207@users.noreply.github.com>
2019-04-05 00:54:52 -0700

    sorter done

--- Unit13/src/NumberSorter.java
+++ Unit13/src/NumberSorter.java
@@ -15,12 +15,40 @@ public class NumberSorter
 	private static int getNumDigits(int number)
 	{
 		int count = 0;
+		int n = number;
+		while (n >0)
+		{
+			n = n/10;
+			count++;
+		}
 		return count;
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int size = getNumDigits(number);
+		int[] sorted = new int[size];
+		int n = number;
+		for (int i = 0; i < size; i ++)
+		{
+			sorted[i] = n % 10;
+			n = n/10;
+		}
+		
+		for (int j = 0; j < size; j++)
+		{
+			for(int k = 0; k < size; k++)
+			{
+				if(sorted[j] < sorted[k])
+				{
+					int setter =0;
+					setter = sorted[j];
+					sorted[j] = sorted[k];
+					sorted[k] = setter;
+				}
+			}
+		}
+		
 		return sorted;
 	}
 }
